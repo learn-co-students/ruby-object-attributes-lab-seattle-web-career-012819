7a7a49dcb74f5cc5d39b895b3b12b7ad4dc2b36d
kwmcminn <kwmcminn@gmail.com>
2019-01-29 17:20:09 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+def initiatize(name, breed)
+  @name = name
+  @breed = breed
+end
+
+def name
+  @name
+end
+
+def name=(name)
+  @name = name
+end
+
+def breed
+  @breed
+end
+
+def breed=(breed)
+  @breed = breed
+end
+end
