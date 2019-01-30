1956cc39ae5d62ebedbf08489128b4e07fb2f852
github username <github email address>
2019-01-29 17:15:55 -0800

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  #getter and setting methods
+    def name
+      @name
+    end
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+    #job setter and getting
+    def job
+      @job
+    end
+
+    def job=(new_job)
+      @job = new_job
+    end
+
+
+end
