de9b4c3bb6c9386fa9a4c2025087cc4ce72a2a3a
github username <github email address>
2019-01-30 07:20:16 -0800

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  def name
+  # method reads name
+    @name
+  end
+
+  def name=(name)
+  # method writes name
+    @name = name
+  end
+
+  def job
+  # method reads job
+    @job
+  end
+
+  def job=(job)
+  # method writes job
+    @job = job
+  end
+end
